65bbc10b2461609f6a0f16e70ce4dd0e3a67a38e
Jakub Hájek <jakub.hajek@skolavdf.cz>
2022-11-22 22:28:58 +0100

    init

---
+++ 1ita.py
@@ -0,0 +1,34 @@
+#Zadanou rychlost v m/s převeď na km/s 
+
+#Jsou dána dvě kladná čísla, která představují délky stran obdelníka
+#v centimetrech. Zjistěte, zda jde o čtverec nebo o obdélník
+
+#určete, zda dvě čísla na vstupu (x, y) splňují nerovnost
+# 4x + 3 < 5y - 1
+
+#na vstupu mám 3 čísla. Vypište největší z nich
+
+# a = 8
+# b = 77
+# c = 16
+
+# if(a>b):
+#     if(a>c):
+#         print(a, "je největší")
+#     else:
+#         print(c, "je největší")
+# else:
+#     if(b>c):
+#         print(b, "je největší")
+#     else:
+#         print(c, "je největší")
+
+# if(a>b and a>c):
+#     print("a", a)
+# elif(b>c):
+#     print("b", b)
+# else:
+#     print("c", c)
+
+
+
